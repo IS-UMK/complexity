1d15aeccf0b552f65463cd68557b4a7735c97dda
Marek Grochowski <grochu@is.umk.pl>
2019-11-29 00:00:00 +0100

    Clean up code

--- MMSE_features/README.md
+++ MMSE_features/README.md
@@ -1,9 +1,9 @@
 # MMSE features
 
 Scripts needed to compute MMSE features
-(RUN EXPLICITLY) mmse_to_feats -> regex_table_vars+extract_features
+(RUN EXPLICITLY) mmse_to_feats -> extract_features
 
 # MMSE distances
 
 Scripts needed to compute distances between MMSE vectors
-(RUN EXPLICITLY) mmse_distances -> regex_table_vars+pdist3
+(RUN EXPLICITLY) mmse_distances -> pdist3
--- MMSE_features/mmse_to_feats.m
+++ MMSE_features/mmse_to_feats.m
@@ -2,7 +2,10 @@ function  f=mmse_to_feats(input_file, output_file, scales)
 
 % Reads CSV file with raw MMSE scales, compute 4 basic features (auc,
 % max_slope, avg_entropy, diff_std) and save results in CSV format.
-    
+%
+% Usage:
+%    x = mmse_to_feats('input.csv'), 'output.csv')
+%
     if nargin < 3
         scales = 1:12;
     end
@@ -11,15 +14,12 @@ function  f=mmse_to_feats(input_file, output_file, scales)
         output_file = 'features.csv';
     end
     
-    features_set = 'basic';
-
     x = readtable(input_file);
-    id_name = x.Properties.VariableNames(1);
-    x.Properties.RowNames = x.(id_name{1});
     
     var_names = x.Properties.VariableNames(2:end);
     mmse_vars = var_names(scales);
+    f = extract_features(x{:, mmse_vars});
     
-    f = extract_features(x, mmse_vars, features_set);
+    f = [x(:, 1), f];
     writetable(f, output_file, 'Delimiter', ';');
 end
