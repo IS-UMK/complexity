6420a94e4f60dba2e88f31ea77ba91f1f08a06d7
Marek Grochowski <grochu@is.umk.pl>
2019-11-29 00:00:00 +0100

    Clean up code

--- MMSE_features/extract_features.m
+++ MMSE_features/extract_features.m
@@ -1,4 +1,4 @@
-function out_t=extract_features(t, selected_vars, features_set)
+function out_t=extract_features(x)
 % For given table contigning entropy scales creates new table with extracted features:
 % * 'auc'         - area under curve
 % * 'max_slope'   - maximum slope value in small scales (original 1-7, here 1-4) 
@@ -6,25 +6,7 @@ function out_t=extract_features(t, selected_vars, features_set)
 % * 'diff_std'    - variation betwen two subsequent scales in the first
 %                   half of the scales
 
-
-if nargin < 2 
-    if istable(t)
-        selected_vars = regex_table_vars(t, '^MMSEValuesFinal_[0-9]+$');
-    else
-        selected_vars = 1:size(t, 2);
-    end
-end
-if nargin < 3 ; features_set = 'basic'; end
-
 out_t = table();
-if istable(t)
-    x = t{:, selected_vars};
-    if ismember('ID', t.Properties.VariableNames)
-        out_t.ID = t.ID;
-    end
-else 
-    x = t(:, selected_vars);
-end
 
 n = size(x, 2);
 
@@ -42,42 +24,6 @@ out_t.Properties.VariableDescriptions{'avg_entropy'} = 'Avg. entropy in high sca
 scales = 1:ceil(n/2);
 out_t.diff_std = diff_std(x, scales);
 out_t.Properties.VariableDescriptions{'diff_std'} = 'Differences variation';
-
-if strcmp(features_set, 'basic')
-    return
-end
-
-out_t.avg_entropy_all = mean(x, 2);
-out_t.Properties.VariableDescriptions{'avg_entropy_all'} = 'Avg. entropy in all scales';
-
-% scales = 1:ceil(n/2);
-% out_t.avg_entropy_low = mean(x(:, scales), 2);
-% out_t.Properties.VariableDescriptions{'avg_entropy_low'} = 'Avg. entropy in low scales 1-6';
-
-out_t.std = std(x, 0, 2);
-out_t.Properties.VariableDescriptions{'std'} = 'Entropy std.';
-
-[m_v, m_v_id] = min(x, [], 2);
-out_t.min = m_v;
-out_t.Properties.VariableDescriptions{'min'} = 'Min entropy';
-
-out_t.min_id = m_v_id;
-out_t.Properties.VariableDescriptions{'min_id'} = 'Id of min entropy';
-
-[m_v, m_v_id] = max(x, [], 2);
-out_t.max = m_v;
-out_t.Properties.VariableDescriptions{'max'} = 'Max entropy';
-
-out_t.max_id = m_v_id;
-out_t.Properties.VariableDescriptions{'max_id'} = 'Max entropy';
-
-% scales = 1:ceil(n/2);
-% [m_v, m_v_id] = min(x(:, scales), [], 2);
-% out_t.min_low = m_v;
-% out_t.Properties.VariableDescriptions{'min_low'} = 'Min entropy in low scales 1:6';
-% 
-% out_t.min_id_low = m_v_id;
-% out_t.Properties.VariableDescriptions{'min_id_low'} = 'ID of min scale in low scales';
 
 end
 
